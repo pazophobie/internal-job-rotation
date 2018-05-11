e6034e6f54cdd32abef48ce365934098af85a96f
pazophobie <apfelbanane@mailbox.org>
2018-05-11 12:20:34 +0200

    the grid to gsc_algorithm

--- main.py
+++ main.py
@@ -8,21 +8,23 @@ pd.set_option('display.width', desired_width)
 xl = pd.ExcelFile('participants_preferences.xlsx')
 
 
-""" define dataframes and make them workable... """
-df1_raw = xl.parse('Sheet1')  # workers
+""" define dataframes """
+
+df1_raw = xl.parse('Workers')  # workers
 df1 = df1_raw.drop([0,1])
 df1 = df1.reset_index()
 df1 = df1.drop(['index'], 1)
 
-df2_raw = xl.parse('Sheet2')  # managers<
+df2_raw = xl.parse('Managers')  # managers<
 df2 = df2_raw.drop([0])
 df2 = df2.reset_index()
 df2 = df2.drop(['index'], 1)
 
-the_grid = xl.parse('Sheet3')  # the grid
+the_grid = xl.parse('Grid')  # the grid
 
 
-""" Create lists of all participants and all managers"""
+""" Create lists of all workers and all managers"""
+
 workers = list(df1.columns.values)
 del workers[0]
 managers = list(df2.columns.values)
@@ -36,33 +38,25 @@ for w in workers:
         volunteers.append(w)
 
 
-#print(df1)
 
 
-"""co"""
+"""COA"""
 from coa import coa
-coa_worker_proposing = coa(workers,managers,df1,df2)
+coa = coa(workers,managers,df1,df2)
 #coa_normal_manager_proposing = coa_normal(managers,workers,df2,df1)
-#print(coa_normal_worker_proposing)
-
-
 
 """ACOA"""
 from acoa import acoa
 acoa = acoa(workers,managers,df1,df2,df1_raw,volunteers)
-#print(acoa)
 
-
-"""THE GRID"""
+"""GSC Algorithm"""
 from gsc_algorithm import gsc_algorithm
-grid_matching, grid_story = gsc_algorithm(workers,managers,df1,df2,the_grid)
-
-
+gsc_matching, gsc_story = gsc_algorithm(workers,managers,df1,df2,the_grid)
 
 
 """Is the outcome stable?"""
 from stability_test import stability_test
-grid_stable, blocking_paris_grid = stability_test(workers,managers,df1,df2,grid_matching)
+gsc_stable, gsc_blocking_paris = stability_test(workers,managers,df1,df2,gsc_matching)
 
 
 
@@ -70,10 +64,10 @@ grid_stable, blocking_paris_grid = stability_test(workers,managers,df1,df2,grid_
 
 """ What to print"""
 
-print('grid outcome:',grid_matching)
-print(grid_story)
-print('The Grid is ', grid_stable)
-print(blocking_paris_grid)
+print('grid outcome:',gsc_matching)
+print(gsc_story)
+print('The Grid is ', gsc_stable)
+print(gsc_blocking_paris)
 
 
 #print('The acoa is ', stability_test(workers,managers,df1,df2,da_adjusted))
